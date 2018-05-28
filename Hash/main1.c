c88af783496a418859916ca324309e3d54bea55b
Cyruswen <Cyrus_wen@163.com>
2018-05-28 16:22:54 +0800

    add main1.c

--- Hash/main1.c
+++ Hash/main1.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "hash1.h"
+#include <stdio.h>
 
 size_t HashFuncDefault(KeyType key){
     return key % HashMaxSize;
@@ -11,9 +11,52 @@ void TestInit(){
     HashInit(&ht, HashFuncDefault);
     printf("size expected 0, actual %lu\n", ht.size);
     printf("func expected %p, actual %p\n", HashFuncDefault, ht.func);
+    return;
+}
+
+void TestHashDestroy(){
+    TestType;
+    HashTable ht;
+    HashInit(&ht, HashFuncDefault);
+    HashDestroy(&ht);
+    printf("size expected 0, actual %lu\n", ht.size);
+    printf("func expected NULL, actual %p\n",ht.func);
+    return;
+}
+
+void TestHashInsert(){
+    TestType;
+    HashTable ht;
+    HashInit(&ht, HashFuncDefault);
+    HashInsert(&ht, 1, 1);
+    HashInsert(&ht, 1, 10);
+    HashInsert(&ht, 2, 2);
+    HashInsert(&ht, 1001, 11);
+    HashInsert(&ht, 1002, 12);
+    HashPrnit(&ht, "插入元素");
+    return;
+}
+
+void TestFind(){
+    TestType;
+    HashTable ht;
+    HashInit(&ht, HashFuncDefault);
+    HashInsert(&ht, 1, 1);
+    HashInsert(&ht, 1, 10);
+    HashInsert(&ht, 2, 2);
+    HashInsert(&ht, 1001, 11);
+    HashInsert(&ht, 1002, 12);
+    int value = 0;
+    int ret = HashFind(&ht, 1002, &value);
+    printf("ret expected is 1, actual %d\n", ret);
+    printf("value epected is 12, actual %d\n", value);
+    return;
 }
 
 int main(){
     TestInit();
+    TestHashDestroy();
+    TestHashInsert();
+    TestFind();
     return 0;
 }
