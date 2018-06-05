49d3b5cf4531786ff3e874e33ad6f847a533b532
Cyruswen <Cyrus_wen@163.com>
2018-06-05 23:18:33 +0800

    add main2.c

--- Hash/main2.c
+++ Hash/main2.c
@@ -1,20 +1,42 @@
 #include "hash2.h"
 #include <stdio.h>
 
-size_t HashFuncDefault(KeyType key){
-    return key % HASHMAXSIZE;
-}
 
 void TestHashInit(){
     TestType;
     HashTable ht;
     HashInit(&ht,HashFuncDefault);
-    printf("func expected %p, actuall %p\n", HashFuncDefault, ht.func);
+    printf("func expected %p, actual %p\n", HashFuncDefault, ht.func);
+    printf("size expected 0, actual %lu\n",ht.size);
+    return;
+}
+
+void TestHashDestroy(){
+    TestType;
+    HashTable ht;
+    HashDestroy(&ht);
+    printf("func expected NULL, actual %p\n", ht.func);
     printf("size expected 0, actuall %lu\n",ht.size);
     return;
 }
 
+void TestHashInsert(){
+    TestType;
+    HashTable ht;
+    HashInit(&ht, HashFuncDefault);
+    HashInsert(&ht, 2, 10);
+    HashInsert(&ht, 10, 13);
+    HashInsert(&ht, 12, 14);
+    HashInsert(&ht, 13, 15);
+    HashPrint(&ht, "打印哈希表");
+    return;
+}
+
+
+
 int main(){
     TestHashInit();
+    TestHashDestroy();
+    TestHashInsert();
     return 0;
 }
