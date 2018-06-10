3a486f69afde476b135eeb48edda19e86aaa646e
Cyruswen <Cyrus_wen@163.com>
2018-06-10 20:03:07 +0800

    updata bin

--- binarysearchtree/binary_search_tree.c
+++ binarysearchtree/binary_search_tree.c
@@ -53,6 +53,17 @@ void TreeInsert(TreeNode** pRoot, TreeNodeType to_insert){
     TreeNode* cur = *pRoot;
     if(to_insert < cur->data){
         TreeInsert(&cur->lchild, to_insert);
-    }else if()
-    
+    }else if(to_insert < cur->data){
+        TreeInsert(&cur->rchild, to_insert);
+    }else{
+        //如果要插入的元素值刚好和现在节点的值相等，有很多种处理方式
+        //这里采取的是不允许二叉树中存在重复的元素
+        //所以如果遇到插入的值刚好和现在结点的值相等的情况
+        //就不采取任何操作，直接返回，插入失败
+        //也可以采取其他的方式：
+        //  1.可以把这个元素放到相等元素的左子树的最右边
+        //  2.可以把这个元素放到相等元素的右子树的最左边
+        return;
+    }
+    return;
 }
